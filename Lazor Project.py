397dcacd1f66725b548148abde0e578632c06519
Ruihao <niruihao@hotmail.com>
2019-10-28 22:06:25 -0400

    basic idea
    some basic idea on how to change the problem to a math / computer language

--- Lazor Project.py
+++ Lazor Project.py
@@ -1,4 +1,92 @@
 # Lazor Project
 
 if __name__ == "__main__":
-	#start the program
+    #start the program
+
+'''
+1. arrange A B C into the empty O site to generate 
+a random distribution of the box.
+
+2. Convert the box into the point with transmission/reflection 
+condition with the coordnation.
+
+3. put the laser ray in and generate all the point which the 
+ray pass through.
+
+4. Check that if all the expected point have a ray come through.
+
+
+Type of variate: list of list
+
+1. Box: 
+
+   box[]
+
+    (1) Center position (cx, cy): 
+        (1,1) (3,1) (5,1) (7,1)
+        (1,3) (3,3) (5,3) (7,3)
+
+    (2) Box type: box_type: A/ B/ C/ o/ x
+    x = no block allowed
+    o = blocks allowed
+    A = fixed reflect block
+    B = fixed opaque block
+    C = fixed refract block
+
+    [cx, cy, boxtype]
+
+2. Grid Point:
+
+    grid_point[]
+
+    (1) Position: (gx, gy)
+
+    (2) orientation (ox, oy): 
+    up (0, -1) / down (0,1) / left (-1, 0) / right (1, 0)
+
+    (3) type: o/ A/ B/ C
+    o: pass the point
+    A: reflect
+    B: terminate the ray
+    C: pass and reflect
+
+    [gx, gy, ox, oy, gridtype]
+
+3. Ray point:
+
+    ray_point[]
+
+    (1) position (rx, ry)
+    (2) direction(vx, vy) 
+
+    [rx, ry, vx, vy] 
+    (same format as the given input laser)
+
+4. Ray:
+
+    convert the ray point to a  straight line function 
+    for (x0, y0)
+    y - (vx * vy)* x = y0 - (vx * vy)* x0
+
+    y = k * x + b
+
+    k = vx * vy
+    b = y0 - vx * vy * x0
+
+    direction on x axis : vx
+
+    [k, b, vx]
+'''
+    
+
+
+
+
+
+
+
+
+
+
+
+
