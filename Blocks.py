2a4213afa978930088d922b6390e7cab945a8ad4
arthurzhang434 <54689336+arthurzhang434@users.noreply.github.com>
2019-11-09 18:30:48 -0500

    Update of Class Block, def update_laser and def find_gp

--- Blocks.py
+++ Blocks.py
@@ -9,68 +9,100 @@ This class contains the functions to account for blocks
 """
 #import numpy as np
 from copy import deepcopy
+#test, first step of Mad7
+grid_points = [[[2,1],[-1,0],'o'], [[9,4],[0,-1],'x']]
+l_start_points = [[[2,1],[1,1]],[[9,4],[-1,1]]]
 
 class Block():
     
-    def __init__(self, grid_points, l_start_points): #type(grid_points,l_start_points)=np.array
-        self.grid_points = grid_points
-        self.l_start_points = l_start_points
-        self.new_l = []
-        for i in range(len(self.grid_points)):
-            if self.grid_points[i][2] == 'o' or 'x':
-                self.new_l.append(self.noblock(self.l_start_points, self.grid_points[i]))
-            if self.grid_points[i][2] == 'A':
-                self.new_l.append(self.reflect(self.l_start_points, self.grid_points[i]))
-            if self.grid_points[i][2] == 'B':
-                self.new_l.append(self.opaque())
-            if self.grid_points[i][2]== 'C':
-                #set 2 variables to account the 2 return values for refract function
-                l1, l2 = self.refract(self.l_start_points, self.grid_points[i])
-                self.new_l.append(l1)
-                self.new_l.append(l2)
-            
-
-    def noblock(self, l_start_points, grid_point):
-        old_l_points = deepcopy(self.l_start_points)
-        for i in range(len(self.l_start_points)):
-            if self.l_start_points[i][0] == grid_point[0]:
-                self.l_start_points[i][0][0] == self.l_start_points[i][0][0] + self.l_start_points[i][1][0]
-                self.l_start_points[i][0][1] == self.l_start_points[i][0][1] + self.l_start_points[i][1][1]
-            else:
-                continue
-        if old_l_points == self.l_start_points:
-            return None
-        if old_l_points != self.l_start_points:
-            return self.l_start_points
+    def __init__(self, grid_point, l_start_point): 
+        self.grid_point = grid_point
+        self.l_start_point = l_start_point
+        self.new_l1 = []
+        self.new_l2 = []
+        #print(self.grid_point[2])
     
-    def reflect(self, l_start_points, grid_point):
-        old_l_points = deepcopy(self.l_start_points)
-        for i in range(len(self.l_start_points)):
-            if self.l_start_points[i][0][0] % 2 != 0:
-                if self.l_start_points[i][0] == grid_point[0] and (self.l_start_points[i][1][1])*(grid_point[1][1])<0:
-                    self.l_start_points[i][1][1] = -self.l_start_points[i][1][1]
-                    self.l_start_points[i][0][0] = self.l_start_points[i][0][0] + self.l_start_points[i][1][0]
-                    self.l_start_points[i][0][1] = self.l_start_points[i][0][1] + self.l_start_points[i][1][1]
-            if self.l_start_points[i][0][0] % 2 == 0:
-                if self.l_start_points[i][0] == grid_point[0] and (self.l_start_points[i][1][0])*(grid_point[1][0])<0:
-                    self.l_start_points[i][1][0] = -self.l_start_points[i][1][0]
-                    self.l_start_points[i][0][0] = self.l_start_points[i][0][0] + self.l_start_points[i][1][0]
-                    self.l_start_points[i][0][1] = self.l_start_points[i][0][1] + self.l_start_points[i][1][1]
-        if old_l_points == self.l_start_points:
-            return None
-        if old_l_points != self.l_start_points:
-            return self.l_start_points
-        
-    def refract(self, l_start_points, grid_point):#set 2 return values, reflect and transmitt
-        old_l_points1 = deepcopy(self.l_start_points)
-        old_l_points2 = deepcopy(self.l_start_points)
+        if self.grid_point[2] == 'o' or 'x':
+            self.new_l1 = self.l_start_point
+        if self.grid_point[2] == 'A':
+            self.new_l1 = self.reflect()
+        if self.grid_point[2] == 'B':
+            self.new_l1 = self.opaque()
+        if self.grid_point[2]== 'C':
+            self.new_l1, self.new_l2 = self.refract()
+        #print(self.new_l1, self.new_l2)    
+    
+    def reflect(self):
+        if self.grid_point[1][0] == 0:
+            self.l_start_point[1][1] = -self.l_start_point[1][1]
+            self.new_l1 = self.l_start_point
+        if self.grid_point[1][0] != 0:
+            self.l_start_point[1][0] = -self.l_start_point[1][0]
+            self.new_l1 = self.l_start_point
+        return self.new_l1
+                    
+    def refract(self):
+        self.new_l2 = deepcopy(self.l_start_point)
         self.reflect()
-        self.noblock()
+        return self.new_l1, self.new_l2
         
-    
     def opaque(self):
         return None
-                    
+#Block(alist,blist)       
+    
+#def find_gp(filename, point):
+def find_gp(grid, point):
+    # filename: in which picture
+    # point e.g. : [[2, 1[], [1, 1]]
+    # grid point [[8, 9], [-1, 0], 'o']
+    
+    # change w, h
+    #w, h = read_bff(filename, 'size')
+    w, h = 15, 15
+    gp = []
+    #print(point[0][0])
+
+    if point[0][0] > 0 and point[0][0] < 2*w and point[0][1] > 0 and point[0][1] < 2*h:
+        #box = read_bff(filename, 'box')
+        #grid = convert_box(filename)
+        if point[0][0] % 2 == 0:
+            face = [-point[1][0], 0]
+        if point[0][0] % 2 != 0:
+            face = [0, -point[1][1]]
+        for i in range(len(grid)):
+            if grid[i][0] == point[0] and grid[i][1] == face:
+                gp = grid[i]
+                break
+        return gp
+    else:
+        return 'out'
+                
+#print(find_gp(grid_points,l_start_points))
+    
+
+def update_laser(grid_points, l_start_points):
+    laser_out_points = []
+    for i in range(len(l_start_points)):
+        grid_point = find_gp(grid_points, l_start_points[i])
+        #print(grid_point)
+        a = Block(grid_point, l_start_points[i])
+        new_l1 = a.new_l1
+        new_l2 = a.new_l2
+        if len(new_l1) != 0:
+            new_l1[0][0] = new_l1[0][0] + new_l1[1][0]
+            new_l1[0][1] = new_l1[0][1] + new_l1[1][1]            
+            laser_out_points.append(new_l1)
+        if len(new_l2) != 0:
+            new_l2[0][0] = new_l2[0][0] + new_l2[1][0]
+            new_l2[0][1] = new_l2[0][1] + new_l2[1][1]
+            laser_out_points.append(new_l2)
+            
+    return(laser_out_points)
+    
+print(update_laser(grid_points, l_start_points))
+        
+    
+
         
             
             
