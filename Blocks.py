b8543c42a81429b2dbf7ad18a34716ab42d960f2
arthurzhang434 <54689336+arthurzhang434@users.noreply.github.com>
2019-11-10 15:10:44 -0500

    Update laser

--- Blocks.py
+++ Blocks.py
@@ -91,12 +91,12 @@ def update_laser(grid_points, l_start_points):
         a = Block(grid_point, l_start_points[i])
         new_l1 = a.new_l1
         new_l2 = a.new_l2
-        if len(new_l1) != 0:
+        if len(new_l1) != 0 and new_l1[1][0] != 0:
             new_l1[0][0] = new_l1[0][0] + new_l1[1][0]
             new_l1[0][1] = new_l1[0][1] + new_l1[1][1]            
             laser_out_points.append(new_l1)
-            
-        if len(new_l2) != 0:
+                
+        if len(new_l2) != 0 and new_l2[1][0] != 0:
             new_l2[0][0] = new_l2[0][0] + new_l2[1][0]
             new_l2[0][1] = new_l2[0][1] + new_l2[1][1]
             laser_out_points.append(new_l2)
