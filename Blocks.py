2aaf00d1688d05788a4b6cd3b190e51c097b0ed5
arthurzhang434 <54689336+arthurzhang434@users.noreply.github.com>
2019-11-10 17:55:26 -0500

    Update blocks

--- Blocks.py
+++ Blocks.py
@@ -10,8 +10,41 @@ This class contains the functions to account for blocks
 #import numpy as np
 from copy import deepcopy
 #test, first step of Mad7
-grid_points = [[[2,1],[-1,0],'o'], [[9,4],[0,-1],'x']]
-l_start_points = [[[2,1],[1,1]],[[9,4],[-1,1]]]
+#grid_points = [[[2,1],[-1,0],'o'], [[9,4],[0,-1],'x']]
+
+grid_points = [[[1, 0], [0, -1], 'o'], [[0, 1], [-1, 0], 'o'], [[1, 2], [0, 1], 'o'], [[2, 1], [1, 0], 'o'], 
+               [[3, 0], [0, -1], 'o'], [[2, 1], [-1, 0], 'o'], [[3, 2], [0, 1], 'o'], [[4, 1], [1, 0], 'o'], 
+               [[5, 0], [0, -1], 'o'], [[4, 1], [-1, 0], 'o'], [[5, 2], [0, 1], 'o'], [[6, 1], [1, 0], 'o'], 
+               [[7, 0], [0, -1], 'o'], [[6, 1], [-1, 0], 'o'], [[7, 2], [0, 1], 'o'], [[8, 1], [1, 0], 'o'], 
+               [[9, 0], [0, -1], 'o'], [[8, 1], [-1, 0], 'o'], [[9, 2], [0, 1], 'o'], [[10, 1], [1, 0], 'o'], 
+               [[1, 2], [0, -1], 'A'], [[0, 3], [-1, 0], 'A'], [[1, 4], [0, 1], 'A'], [[2, 3], [1, 0], 'A'], 
+               [[3, 2], [0, -1], 'o'], [[2, 3], [-1, 0], 'o'], [[3, 4], [0, 1], 'o'], [[4, 3], [1, 0], 'o'], 
+               [[5, 2], [0, -1], 'A'], [[4, 3], [-1, 0], 'A'], [[5, 4], [0, 1], 'A'], [[6, 3], [1, 0], 'A'], 
+               [[7, 2], [0, -1], 'o'], [[6, 3], [-1, 0], 'o'], [[7, 4], [0, 1], 'o'], [[8, 3], [1, 0], 'o'], 
+               [[9, 2], [0, -1], 'A'], [[8, 3], [-1, 0], 'A'], [[9, 4], [0, 1], 'A'], [[10, 3], [1, 0], 'A'], 
+               [[1, 4], [0, -1], 'o'], [[0, 5], [-1, 0], 'o'], [[1, 6], [0, 1], 'o'], [[2, 5], [1, 0], 'o'], 
+               [[3, 4], [0, -1], 'A'], [[2, 5], [-1, 0], 'A'], [[3, 6], [0, 1], 'A'], [[4, 5], [1, 0], 'A'], 
+               [[5, 4], [0, -1], 'o'], [[4, 5], [-1, 0], 'o'], [[5, 6], [0, 1], 'o'], [[6, 5], [1, 0], 'o'], 
+               [[7, 4], [0, -1], 'o'], [[6, 5], [-1, 0], 'o'], [[7, 6], [0, 1], 'o'], [[8, 5], [1, 0], 'o'], 
+               [[9, 4], [0, -1], 'x'], [[8, 5], [-1, 0], 'x'], [[9, 6], [0, 1], 'x'], [[10, 5], [1, 0], 'x'], 
+               [[1, 6], [0, -1], 'o'], [[0, 7], [-1, 0], 'o'], [[1, 8], [0, 1], 'o'], [[2, 7], [1, 0], 'o'], 
+               [[3, 6], [0, -1], 'o'], [[2, 7], [-1, 0], 'o'], [[3, 8], [0, 1], 'o'], [[4, 7], [1, 0], 'o'], 
+               [[5, 6], [0, -1], 'o'], [[4, 7], [-1, 0], 'o'], [[5, 8], [0, 1], 'o'], [[6, 7], [1, 0], 'o'], 
+               [[7, 6], [0, -1], 'o'], [[6, 7], [-1, 0], 'o'], [[7, 8], [0, 1], 'o'], [[8, 7], [1, 0], 'o'], 
+               [[9, 6], [0, -1], 'o'], [[8, 7], [-1, 0], 'o'], [[9, 8], [0, 1], 'o'], [[10, 7], [1, 0], 'o'],
+               [[1, 8], [0, -1], 'o'], [[0, 9], [-1, 0], 'o'], [[1, 10], [0, 1], 'o'], [[2, 9], [1, 0], 'o'], 
+               [[3, 8], [0, -1], 'o'], [[2, 9], [-1, 0], 'o'], [[3, 10], [0, 1], 'o'], [[4, 9], [1, 0], 'o'], 
+               [[5, 8], [0, -1], 'A'], [[4, 9], [-1, 0], 'A'], [[5, 10], [0, 1], 'A'], [[6, 9], [1, 0], 'A'], 
+               [[7, 8], [0, -1], 'o'], [[6, 9], [-1, 0], 'o'], [[7, 10], [0, 1], 'o'], [[8, 9], [1, 0], 'o'], 
+               [[9, 8], [0, -1], 'A'], [[8, 9], [-1, 0], 'A'], [[9, 10], [0, 1], 'A'], [[10, 9], [1, 0], 'A']]
+
+#grid_points = [[[2, 1], [-1, 0], 'o'], [[9, 4], [0, -1], 'x']]
+l_start_points = [[[2, 1], [1, 1]], [[9, 4], [-1, 1]]]
+
+#l_start_points = [[[5,0],[1,-1]],[[0,9],[-1,1]]]
+end_points = [[6, 3], [6, 5], [6, 7], [2, 9], [9, 6]]
+
+#l_start_points = [[[2,1],[1,1]],[[9,4],[-1,1]]]
 
 class Block():
     
@@ -21,6 +54,7 @@ class Block():
         self.new_l1 = []
         self.new_l2 = []
         #print(self.grid_point[2])
+        #print(self.l_start_point)
     
         if self.grid_point[2] == 'o' or 'x':
             self.new_l1 = self.l_start_point
@@ -31,6 +65,7 @@ class Block():
         if self.grid_point[2]== 'C':
             self.new_l1, self.new_l2 = self.refract()
         #print(self.new_l1, self.new_l2)    
+        
     
     def reflect(self):
         if self.grid_point[1][0] == 0:
@@ -61,9 +96,10 @@ def find_gp(grid, point):
     
     # change w, h
     #w, h = read_bff(filename, 'size')
-    w, h = 15, 15
+    w, h = 5, 5
     gp = []
-    #print(point[0][0])
+    #print(point[0][0],point[0][1])
+    
 
     if point[0][0] > 0 and point[0][0] < 2*w and point[0][1] > 0 and point[0][1] < 2*h:
         #box = read_bff(filename, 'box')
@@ -77,14 +113,17 @@ def find_gp(grid, point):
                 gp = grid[i]
                 break
         return gp
-    else:
-        return 'out'
-                
+    if point[0][0] == 0 or point[0][0] == 2*w or point[0][1] == 0 or point[0][1] == 2*h:
+        gp = [[],[],'B']
+        return gp
+    
+ 
 #print(find_gp(grid_points,l_start_points))
     
 
 def update_laser(grid_points, l_start_points):
     laser_out_points = []
+    
     for i in range(len(l_start_points)):
         grid_point = find_gp(grid_points, l_start_points[i])
         #print(grid_point)
@@ -103,5 +142,24 @@ def update_laser(grid_points, l_start_points):
             
     return(laser_out_points)
     
-print(update_laser(grid_points, l_start_points))
+def test_solution(grid_points, l_start_points):
+    laser_path_point = []
+
+    while len(l_start_points) > 0:
+        laser_path_point = laser_path_point + l_start_points
+        print(laser_path_point)
+        l_start_points = update_laser(grid_points, l_start_points)
+        print(l_start_points)
+        
+            
+    return laser_path_point
+
+print(test_solution(grid_points,l_start_points))
+#print(update_laser(grid_points, l_start_points))
+
+            
+
+    
+    
+    
         
