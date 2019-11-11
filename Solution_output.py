b3cbffb94ba72c2fa512af2b05e2382114b80eb0
arthurzhang434 <54689336+arthurzhang434@users.noreply.github.com>
2019-11-11 14:33:40 -0500

    Update

--- Solution_output.py
+++ Solution_output.py
@@ -8,15 +8,13 @@ Created on Sun Nov 10 10:57:01 2019
 '''
 This file contains the function to visualize the output of the solution
 '''
-from PIL import Image, ImageDraw
+from PIL import Image
 
-solution = [['o','o','C','o'],
-       ['o','o','o','A'],
-       ['A','o','o','o'],
-       ['o','o','o','o']
-       ] 
+solution0 = [[[1, 1], 'o'], [[3, 1], 'o'], [[5, 1], 'C'], [[7, 1], 'o'], 
+             [[1, 3], 'o'], [[3, 3], 'o'], [[5, 3], 'o'], [[7, 3], 'A'], 
+             [[1, 5], 'A'], [[3, 5], 'o'], [[5, 5], 'o'], [[7, 5], 'o'], 
+             [[1, 7], 'o'], [[3, 7], 'o'], [[5, 7], 'o'], [[7, 7], 'o']]
 
-blocks = [[3,0],[4,1],[5,2],[6,3],[5,4],[4,5],[3,6],[2,7],[4,3],[3,4],[2,5],[3,6],[4,7],[5,8]]
 
 colors ={'o':(255,255,255),
          'x':(0,0,0),
@@ -34,26 +32,31 @@ def set_colors(img, x0, y0, dim, gap, color):
                     )
             
 
-def solution_output(solution, dim=20, gap=2):
+def solution_output(solution0, dim=50, gap=5):
+    solution1  = []
+    solution2 = []
+    x = 0
+    for j in range(int(len(solution0)/4)):
+        for i in range(int(len(solution0)/4)):
+            solution1.append(solution0[4*x + i][1])
+        solution2.append(solution1)
+        x = x + 1
+        solution1 = []
     
-    w_blocks = len(solution[0])
-    h_blocks = len(solution)
+    w_blocks = len(solution2[0])
+    h_blocks = len(solution2)
     size = (w_blocks*(dim+gap) + gap, h_blocks*(dim+gap) + gap)
     img = Image.new("RGB", size, color=colors['gap'])
+
     
-    for y1, y2 in enumerate(solution):
+    for y1, y2 in enumerate(solution2):
         for x1, x2 in enumerate(y2):
             set_colors(img, x1, y1, dim, gap, colors[x2])
             
-    draw = ImageDraw.Draw(img)
-    
-    for y11, y22 in enmuerate(blocks):
-        for x11, x22 in enmuerate(y22):
-            draw.line((()))
+    img.show()
             
-            
-    img.save('Solution.png')
+    #img.save('Solution.png')
         
-solution_output(solution)
-    
+solution_output(solution0)
+   
 
