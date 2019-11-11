0fe0fc2477e312f52f946e59f489f61c6991e5d0
niruihao <49870508+niruihao@users.noreply.github.com>
2019-11-10 19:08:19 -0500

    add a function to tackle with the maps with two laser

--- Lazor Project.py
+++ Lazor Project.py
@@ -386,39 +386,81 @@ def solve(filename):
                             # check whether all the point demanded have light passed through
                             # record the grid
 
-    # if len(read_bff(filename, 'sp')) == 2:
-    # remove the box if two laser cross
-    #         line2 = first_line(filename, 2)
+if len(read_bff(filename, 'sp')) == 2:.
+    line2 = first_line(filename, 2)
+        if solved == False:#while
+            for br in refl: # select an order of A C arrangement
+                    # select on and put in(save the box)
+                for f1 in line1: # select a box in the first line
+                    print('f1')
+                    print(f1)
+                    # [[1,3],'o']
+                    # change the box
+                    # combination
+                    grid_x = deepcopy(import_box)
+                    # this part use a for loop to find the same box, can be upgraded
+                    for i in range(len(grid_x)):
+                        if grid_x[i][0] == f1[0]:
+                            grid_x[i][1] = br[0]
+                            break
+                    boxoption1 = deepcopy(obox)
+                    boxoption1.remove(f1)
 
-    # elif len(read_bff(filename, 'sp')) == 2:
-    #     while solved == False:
-    #         for f1 in line1:
-    #             print(f1)
-    #             # [[1,3],'o']
-    #             for br1 in refl:
-    #                 # select on and put in(save the box)
-    #                 # update refl(delete the box which put inside)
-    #                 # change the box
-    #                 # combination
-    #                 # update line2 in case two line cross
-    #                 for f2 in line2:
-    #                     print(f2)
-    #                     # [[1,3],'o']
-    #                     for br2 in refl:
-    #                         # select on and put in(save the box)
-    #                         # update refl(delete the box which put inside)
-    #                         # change the box
-    #                         # combination
+                    if f1 in line2:
+                        line2.remove(f1)
 
-    #                         # put the other boxes in
-    #                         for box_op in range(len(obox)):
-    #                             # record the used box
-    #                             # update the grid
 
-    #                             #print the grid
-    #                             for box1 in range(len(refl)):
-    #                                 # record the grid
-    
+
+                    for f2 in line2: # select a box in the first line
+                        print('f2')
+                        print(f2)
+                        # [[1,3],'o']
+                        # change the box
+                        # combination
+                        grid_1 = deepcopy(grid_x)
+                        # this part use a for loop to find the same box, can be upgraded
+                        for i in range(len(grid_1)):
+                            if grid_1[i][0] == f1[0]:
+                                grid_1[i][1] = br[0]
+                                break
+                        boxoption = deepcopy(boxoption1)
+                        boxoption.remove(f2)
+                    
+
+
+                        print('grid_1')
+                        print(grid_1)
+                        print('boxoption')
+                        print(boxoption)
+                        boxoption = list(itertools.combinations(boxoption, len(br)-1)) # after put first box, the rest available box
+
+
+                        # variable used by code below
+                        # box option; grid 1, 
+                        # put the other boxes in
+                        for box_op in range(len(boxoption)): # select an order of combination
+                            usedbox = [br[0]]
+                            grid_2 = deepcopy(grid_1) # copy the grid with the first box put in
+                            # record the used box
+                            # update the grid
+                            # print the grid
+                            for box1 in range(len(br)-1):
+                                print('br =', br)
+                                print('boxoption[box_op]')
+                                print(boxoption[box_op])
+                                for i in range(len(grid_2)):
+                                    # print(1, grid_2[i][0])
+                                    # print(2, boxoption[box_op][box1])
+                                    if grid_2[i][0] == boxoption[box_op][box1][0]:
+                                        grid_2[i][1] = br[box1+1]
+                                        break
+                                usedbox.append(br[box1+1])
+                                print('used box')
+                                print(usedbox)
+                                print('i')
+                                print(i)
+                                print('grid_2')
+                                print(grid_2)    
     # for B in box_left:
     #     copy the grid solved
     #     assign B box
@@ -434,17 +476,18 @@ def solve(filename):
 
 if __name__ == "__main__":
     # solve('mad_1.bff')
-    a = convert_box('mad_7_test.bff')
-    print(a)
-    ep = read_bff('mad_7.bff', 'ep')
-    print(ep)
+    # a = convert_box('mad_7_test.bff')
+    # print(a)
+    # ep = read_bff('mad_7.bff', 'ep')
+    # print(ep)
     # b = read_bff('mad_7.bff', 'box')
     # print(b)
     # print(find_gp(b[1], 'mad_7.bff'))
     # print(first_line('mad_7.bff', 1))
     # c = first_line(1, 'mad_7.bff')
     # print(c)
-    
+    a = [1,2,3,4,5]
+    print(6 in a)
 
 
     # for i in range(len(a)):
